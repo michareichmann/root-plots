b5939e8b82ebbc90ada6de80432f0c13a9adb828
michareichmann <micha.reichmann@gmail.com>
2022-12-04 15:45:39 +0100

    allow array for leg_tits

--- draw.py
+++ draw.py
@@ -667,7 +667,7 @@ class Draw(object):
         h0 = histos[0]
         self.histo(s, show=False, save=False, draw_opt='nostack')
         format_histo(s, **prep_kw(dkw, x_tit=h0.GetXaxis().GetTitle(), y_tit=h0.GetYaxis().GetTitle(), **Draw.mode(1, y_off=h0.GetYaxis().GetTitleOffset())))
-        leg = self.legend(histos, leg_titles, ldraw, w=w) if leg_titles else None
+        leg = self.legend(histos, leg_titles, ldraw, w=w) if leg_titles is not None else None
         self.histo(s, **prep_kw(dkw, draw_opt='nostack', leg=leg, lm=get_last_canvas().GetLeftMargin()))
         return s
 
